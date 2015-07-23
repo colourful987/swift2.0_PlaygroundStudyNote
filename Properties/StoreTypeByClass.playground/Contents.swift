3cf55745bf2384d2bade984b028284e042d89222
colourful987 <mcxcode@163.com>
2015-07-23 14:14:12 +0800

    明确了最新类型属性语法

--- Properties/StoreTypeByClass.playground/Contents.swift
+++ Properties/StoreTypeByClass.playground/Contents.swift
@@ -4,11 +4,16 @@ import UIKit
 
 class MyClass{
   //这和书中 For classes, you can define computed type properties only 相悖！
-  //谁知道请告诉我
+  //早在swift1.2改动中就加入了class type properties的stored properties
+  //因此书中是未修改的。
   static var storedClassProp = "Hey It's My class"
   //这个是可以重载的 要用class 关键字修饰
   class var computedTypeProp : Int {
     return 1
+  }
+  //用class 关键字可以重写 用static 表示只属于这个类的 不能被重写
+  class func sayHello(){
+    print("hello")
   }
 }
 class SubMyClass:MyClass{
@@ -17,6 +22,9 @@ class SubMyClass:MyClass{
     return 2    //重写了不会报错
   }
   
+  override class func sayHello(){
+    print("hello maoxian")
+  }
 }
 
 MyClass.storedClassProp
